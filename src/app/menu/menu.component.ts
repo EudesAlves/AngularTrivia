2c1b8ea62a67c8b51ad1c742d5c5ba878e886a3c
Eudes <eudes.eds@gmail.com>
2025-04-02 10:41:55 -0300

    Testes com Diretivas no app Menu.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,28 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-menu',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-  id = 1;
-  name = 'José';
-  imageFileName = 'inazuma1.jpg';
+  id:number = 1;
+  name:string = 'José';
+  imageFileName:string = 'images/inazuma1.jpg';
+  divSquareEnabled:boolean = true;
+  btnSquareText:string = 'Esconder';
+  categories:string[] = ['Filme', 'Musica', 'Geografia', 'Jogos Eletrônicos', 'Cultura Geek'];
+  selectedCategory:string = ''; 
 
   showData() {
     alert('id: ' + this.id.toString() + ' nome: ' + this.name);
   }
 
   changeImage() {
-    if(this.imageFileName === 'inazuma1.jpg') {
-      this.imageFileName = 'inazuma2.jpg';
+    // === : compara valor e tipo da variavel
+    if(this.imageFileName === 'images/inazuma1.jpg') {
+      this.imageFileName = 'images/inazuma2.jpg';
     }
     else {
-      this.imageFileName = 'inazuma1.jpg';
+      this.imageFileName = 'images/inazuma1.jpg';
     }
+  }
+
+  showHideDivSquare():void {
+    if(this.divSquareEnabled) {
+      this.divSquareEnabled = false;
+      this.btnSquareText = 'Exibir';
+    }
+    else {
+      this.divSquareEnabled = true;
+      this.btnSquareText = 'Esconder';
+    }
+  }
+
+  selectCategory(category:string):void {
+    this.selectedCategory = category;
   }
 
 }
